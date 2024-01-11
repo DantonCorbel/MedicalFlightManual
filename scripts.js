e2ff215ba7c9efd7c8ca2c5580ee6218b0c5209a
DantonCorbel <30301067+DantonCorbel@users.noreply.github.com>
2024-01-11 23:20:25 +1300

    try fetch json

--- scripts.js
+++ scripts.js
@@ -8,3 +8,16 @@ const subjectArray = ["Respiratory", "Cardiology", "General"]
 for (let subject in subjectArray) {
   container.innerHTML += `<p>${subjectArray[subject]}<p>`
 }
+
+fetch('./jsonFolder/toc.json')
+        .then(res => res.json())
+        .then((data) => {
+            console.log(data);
+        }).catch(err => console.error(err));
+
+const toc = fetch('./jsonFolder/toc.json')
+        .then(res => res.json())
+
+for (let subject in toc) {
+  container.innerHTML += `<p>${toc[subject]}<p>`
+}
